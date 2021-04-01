3847f86ec06432d1ea3b187462d827adf7126de5
SB <sb@SARARATs-MacBook-Pro.local>
2021-04-01 00:53:59 -0400

    style background, and header

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.border}>
+        <View>
+          <LinearGradient 
+            colors={['#dbb73b', '#f2e4b7']} 
+            style={styles.headBG}>
+          {/******  Header ******/}
+            <Text>Green Creative</Text>
+            <Text>Inspired by Creative Commons, licensed under Creative Commons.</Text>
+          </LinearGradient>
+        </View>
+        <View>
+          {/******  Nav ******/}
+        </View>
+        <View>
+          {/******  Body ******/}
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +30,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#679159',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  border:{
+    backgroundColor: 'rgba(250,250,250, 0.5)',
+    padding: 12,
+    borderRadius: 20,
+    width: '80%',
+    
+  },
+  headBG: {
+    flex: 1,
+    padding: 15,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
   },
 });
