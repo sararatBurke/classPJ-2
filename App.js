bfc0103c9d645e7e01de8dcc624420743b561f0b
SB <sb@SARARATs-MacBook-Pro.local>
2021-04-01 01:46:32 -0400

    added body content and bg

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
@@ -21,8 +21,7 @@ export default function App() {
           {/******  Nav ******/}
           <LinearGradient
             colors={['#866e18', '#dab536']}
-            style={styles.navBG}
-          >
+            style={styles.navBG}>
             <Text>home</Text>
             <Text>about</Text>
             <Text>portfolio</Text>
@@ -34,8 +33,21 @@ export default function App() {
           </LinearGradient>
         </View>
 
-        <View>
+        <View style={styles.bodyGB}>
           {/******  Body ******/}
+          <Text style={styles.bodyHead}>Template Usage</Text>
+            <Text style={styles.bodyText}>You may use this template on any site, anywhere, for free just please leave the link back to me in the footer. This template validates XHTML Strict 1.0, CSS Validates as well; enjoy `:)`</Text>
+            <Text style={styles.bodyText}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque vitae sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor sapien. Praesent leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet imperdiet nunc. Donec commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis risus ut felis. Sed vehicula pellentesque quam.</Text>
+            
+            <TouchableOpacity>
+              <Text>read more</Text>
+            </TouchableOpacity>
+
+          <Text style={styles.bodyHead}>Another Title Goes Here!</Text>
+            <Text style={styles.bodyText}>This particular template goes not have a naviagation panel; it was intended for simple sites. I am also making a version of this template with navigation, check out BryantSmith.com to get it.</Text>
+
+          <Text style={styles.bodyHead}>Yet Another?</Text>
+            <Text style={styles.bodyText}>Each title is an H1 tag, so choose them carefully `:)`</Text>
         </View>
       </View>
     </View>
@@ -62,6 +74,24 @@ const styles = StyleSheet.create({
     padding: 15,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
+  },
+
+  bodyGB:{
+    backgroundColor: '#fff',
+    padding: 15,
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15
+  },
+  bodyHead:{
+    fontFamily: 'Helvetica',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  bodyText:{
+    fontFamily: 'Verdana',
+    fontSize: 12,
+    lineHeight: 25,
+    marginBottom: 20,
   },
 
   navBG:{
